3ca92349f520f8a1dafff5cb48ac39ec286b357c
usama3105 <um354626@gmail.com>
2024-06-02 18:38:41 +0500

    countdown code

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+// countdown.ts
+import inquirer from 'inquirer';
+async function startCountdown() {
+    const { duration } = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'duration',
+            message: 'Enter the countdown duration in seconds:',
+            validate: (input) => input > 0,
+        },
+    ]);
+    let counter = duration;
+    const intervalId = setInterval(() => {
+        counter--;
+        console.log(`Time remaining: ${counter} seconds`);
+        if (counter === 0) {
+            clearInterval(intervalId);
+            console.log('Countdown complete!');
+        }
+    }, 1000);
+}
+startCountdown();
